34828c0a057f9982e07cd5ce39ee047950936169
xztaityozx <taityonohibi@gmail.com>
2017-08-23 00:41:34 +0900

    add py/branch.py

---
+++ py/branch.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+
+argv = os.getenv("CDX_DEFAULT_OPTS", "").split(" ") + sys.argv[1:]
+
+path=sys.argv[len(sys.argv)-1].split("/")
+last=path[len(path)-1]
+
+if any(opt in argv for opt in ['-h',]):
+    print(0)
+elif any(opt in argv for opt in ['-b',]):
+    print(1)
+elif last == '**':
+    print(2)
+elif any(opt in argv for opt in ['--make','--automake']):
+    print(3)
+else:
+    print(4)
--- py/optanalyze.py
+++ py/optanalyze.py
@@ -1,17 +1,29 @@
 import sys
 import os
 import subprocess
+import re
+
+import cdx_color
 
 argv = sys.argv[1:]
+
+shell = os.environ["SHELL"]
 
 defaultOpt = os.environ.get("CDX_DEFAULT_OPTS", "")
 HOME = os.environ["HOME"]
 
-current = subprocess.check_output("pwd").decode('utf-8').replace('\n', '')
+current = subprocess.check_output("pwd").decode('utf-8').replace('\n', '')+"/"
 
 argv = defaultOpt.split(" ") + argv
 
 rt = ""
+
+
+def export(name, value):
+    if shell == "/bin/bash":
+        return name + "='" + value + "';"
+    elif shell == "/usr/bin/fish":
+        return "set " + name + " " + value
 
 # return :
 # 0:exit
@@ -21,25 +33,60 @@ rt = ""
 # 4:--help
 # else:option
 
+
+command = ""
+
+ssh = False
+make = False
+ls=False
+automake=False
+changeTo = current
+
+cdcommand="pushd"
+
 for opt in argv:
-    if opt == "+b":
-        with open(HOME + '/.cdx.bookmark', 'a') as f:
-            f.write(current + "\n")
-        print(0)
-        sys.exit()
-    elif opt == "-p":
-        print(1)
-        sys.exit(1)
-    elif opt == "--ssh":
-        print(2)
-        sys.exit()
-    elif opt == "--help":
-        # print help
-        print(4)
-        sys.exit()
-    elif any(opt == x for x in ['-h', '-b', '--make', '--ssh', '--automake', '--fuzzy', '--ls', '--cd']):
-        rt += opt+" "
+    if opt == '--ssh':
+        ssh = True
+    elif opt == '-p':
+        command += "popd;"
+    elif opt == '--ls':
+        ls=True
+    elif opt == '--make':
+        make=True
+    elif opt == '--automake':
+        make=True
+        automake=True
+    elif opt=='--cd':
+        cdcommand="cd"
     else:
-        print(3)
-        sys.exit()
-print(rt)
+        changeTo=opt
+
+
+if ssh:
+    if os.path.exists(HOME + "/.ssh/config"):
+        check = subprocess.check_output(
+            "cat " + HOME + "/.ssh/config|grep ^Host|awk '{print $2}'|grep " + changeTo, shell=True)
+        if check != "":
+            command = "ssh " + changeTo
+            sys.exit()
+
+#if os.system("cd "+changeTo+" 2> /dev/null 1 > /dev/null") != 0:
+#    if make:
+#        if automake:
+#            os.mkdir(current+changeTo)
+#            command+="echo -e \""+cdx_color.cyan+"Directory has made automatically by --automake option"+cdx_color.reset+"\";"
+#            command+=cdcommand+" "+changeTo+" > /dev/null;"
+#        else:
+#            os.system("echo \""+cdx_color.cyan+changeTo+" is not exists at current direcory. \n Do you want to make direcory?(y/n) "+cdx_color.reset+"\"")
+#            ans=input(">>>" )
+#            if ans=='y':
+#                os.mkdir(current+changeTo)
+#                command+=cdcommand+" "+changeTo+" > /dev/null"
+#            else:
+#                command=""
+
+
+if ls:
+    command+="ls;"
+
+print(command)
